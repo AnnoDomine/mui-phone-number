95a3bbe4d7bd7ac614b5a7780f253886a717e856
dominic.seel <dominic.seel@futurenext.de>
2026-01-01 15:30:38 +0100

    test: improve test infrastructure and add hook unit tests

--- vite.config.mjs
+++ vite.config.mjs
@@ -52,26 +52,37 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
+    projects: [
+      {
+        name: 'unit',
+        extends: true,
+        test: {
+          environment: 'jsdom',
+          include: ['src/**/*.test.{ts,tsx}'],
         },
-        setupFiles: ['.storybook/vitest.setup.js']
+      },
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook')
+          })
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [{
+              browser: 'chromium'
+            }]
+          },
+          setupFiles: ['.storybook/vitest.setup.js']
+        }
       }
-    }]
+    ]
   }
 });
